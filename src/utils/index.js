22a8ca790297c7b9dce5cca8a1fd79c557a6c1a3
bakasibruceto <abla09027@gmail.com>
2024-06-23 01:37:24 +0800

    chore: removed uncessary files

--- src/utils/index.js
+++
@@ -1,5 +0,0 @@
-// export { fetchNewsForCondition } from './HealthNews/NewsApi.js';
-// export { scrapeArticleContent, scrapeMultipleArticles } from './scraper.js';
-// export { findBestSourceForCondition } from './filter.js';
-// export { generateKeywordArray } from './dictionary.js';
-export {fetchHealthAZData}from './nhs.js';
