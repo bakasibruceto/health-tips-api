97425bfcf5ac07e34424f798c264444d3f53693d
bakasibruceto <abla09027@gmail.com>
2024-07-11 23:26:16 +0800

    feat: added more conditions

--- config.py
+++ config.py
@@ -3,5 +3,16 @@ class Config:
     NHS_API_BASE_URL = "https://api.nhs.uk/live-well/"
     USER_AGENT = "Mozilla/5.0"
     CONTENT_TYPE = "application/json"
-    CONDITIONS = ["alcohol-advice", "sexual-health"]
-    
+    CONDITIONS = [
+        "alcohol-advice",
+        "bone-health",
+        "eat-well/food-types",
+        "eat-well/how-to-eat-a-balanced-diet",
+        "eat-well/5-a-day",
+        "eat-well/digestive-health",
+        "exercise",
+        "sexual-health",
+        "healthy-teeth-and-gums",
+        "healthy-weight/managing-your-weight",
+        "pain",
+    ]
