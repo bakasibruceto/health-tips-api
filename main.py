1511619cb9541a8a61a55b0ebeaf25032c70a175
bakasibruceto <abla09027@gmail.com>
2024-08-16 02:37:01 +0800

    last commit

--- main.py
+++ main.py
@@ -1,27 +1,23 @@
-from fastapi import FastAPI, HTTPException, Request
-from fastapi.middleware.cors import CORSMiddleware
-from fastapi import FastAPI, Query
-from sumy.parsers.plaintext import PlaintextParser
-from sumy.parsers.html import HtmlParser
-from sumy.nlp.tokenizers import Tokenizer
-from sumy.nlp.stemmers import Stemmer
-from sumy.utils import get_stop_words
-import nltk
-import os
-import requests
-import random
-from dotenv import load_dotenv
-from pydantic import BaseModel
-from typing import Optional, List
 import asyncio
+from dotenv import load_dotenv
+from fastapi import FastAPI, HTTPException, Query
+from fastapi.middleware.cors import CORSMiddleware
+
+# ---------Start copy from here---------
 import json
+import random
 from datetime import datetime, timezone
-from bs4 import BeautifulSoup
+from typing import List, Optional
+import nltk
+import requests
+from pydantic import BaseModel
+from sumy.nlp.stemmers import Stemmer
+from sumy.summarizers.lex_rank import LexRankSummarizer as Summarizer
+from sumy.nlp.tokenizers import Tokenizer
+from sumy.parsers.html import HtmlParser
+# ---------End copy here----------
 
-# Load environment variables
 load_dotenv()
-
-# Initialize FastAPI app
 app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
@@ -31,79 +27,53 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-# Load configuration
-from config import Config
-    
-app.config = Config()
-
-
 async def main():
     await get_data()
 
-
-# Ensure NLTK resources are downloaded
+# ---------Start copy from here---------
+# Modify the code if needed
 @app.on_event("startup")
 async def download_nltk_resources():
     nltk.download("punkt")
 
-
 class SummarizeRequest(BaseModel):
     data: List[Optional[str]]
 
-
-@app.get("/")
-async def index():
-    return {"message": "Hello!"}
-
-
 @app.post("/summarize")
 async def summarize(request: SummarizeRequest):
-    method, language, sentence_count, input_type, input_, *rest = request.data
-
-    if method == "LSA":
-        from sumy.summarizers.lsa import LsaSummarizer as Summarizer
-    elif method == "text-rank":
-        from sumy.summarizers.text_rank import TextRankSummarizer as Summarizer
-    elif method == "lex-rank":
-        from sumy.summarizers.lex_rank import LexRankSummarizer as Summarizer
-    elif method == "edmundson":
-        from sumy.summarizers.edmundson import EdmundsonSummarizer as Summarizer
-    elif method == "luhn":
-        from sumy.summarizers.luhn import LuhnSummarizer as Summarizer
-    elif method == "kl-sum":
-        from sumy.summarizers.kl import KLSummarizer as Summarizer
-    elif method == "random":
-        from sumy.summarizers.random import RandomSummarizer as Summarizer
-    elif method == "reduction":
-        from sumy.summarizers.reduction import ReductionSummarizer as Summarizer
-
-    if input_type == "URL":
-        parser = HtmlParser.from_url(input_, Tokenizer(language))
-    elif input_type == "text":
-        parser = PlaintextParser.from_string(input_, Tokenizer(language))
-
+    language = "english"
+    sentence_count, input_, *rest = request.data
+    parser = HtmlParser.from_url(input_, Tokenizer(language))
     stemmer = Stemmer(language)
     summarizer = Summarizer(stemmer)
-    stop_words = get_stop_words(language)
-
-    if method == "edmundson":
-        summarizer.null_words = stop_words
-        summarizer.bonus_words = parser.significant_words
-        summarizer.stigma_words = parser.stigma_words
-    else:
-        summarizer.stop_words = stop_words
-
     summary_sentences = summarizer(parser.document, sentence_count)
     summary = " ".join([str(sentence) for sentence in summary_sentences])
-
+    
     return summary
+
+@app.get("/health_tips")
+async def health_tips(param: str):
+    data = await get_list_and_random(param)
+    id = data.get("RandomId")
+    result = await get_data(id)
+
+    summary_request = SummarizeRequest(
+        data=["2", result["link"]]
+    )
+    summary = await summarize(summary_request)
+
+    result["content"] = summary
+    
+    # Debug
+    print(json.dumps(result, indent=5))
+    return result
 
 async def get_data(topic_id: int):
     url = f"https://health.gov/myhealthfinder/api/v3/topicsearch.json?TopicId={topic_id}&Lang=en"
     response = requests.get(url)
     if response.status_code == 200:
         data = response.json()
-        # Navigate through the keys: Result -> Resources -> Resource -> Sections -> section
+        # Navigate through the keys: Result -> Resources -> Resource 
         result = data.get("Result", {})
         resources = result.get("Resources", {})
         resource = (
@@ -111,21 +81,15 @@ async def get_data(topic_id: int):
             if isinstance(resources.get("Resource"), list) and resources.get("Resource")
             else None
         )
+        
         if resource:
-            sections = resource.get("Sections", {}).get("section", {})
-            if sections:
-                first_section = sections[0]
-                content = first_section.get('Content', 'No Content')
-                title = resource.get("Title")
-                lastUpdated = resource.get("LastUpdate")
-                link = resource.get("AccessibleVersion")
-                lastUpdatedInt = int(lastUpdated)
-                lastUpdatedDate = datetime.fromtimestamp(lastUpdatedInt, timezone.utc)
-                lastUpdatedFormatted = lastUpdatedDate.strftime("%B %d, %Y")
-                soup = BeautifulSoup(content, "html.parser")
-                clean_content = soup.get_text()
-            else:
-                return {"error": "Sections list is empty"}
+            title = resource.get("Title")
+            lastUpdated = resource.get("LastUpdate")
+            link = resource.get("AccessibleVersion")
+            lastUpdatedInt = int(lastUpdated)
+            lastUpdatedDate = datetime.fromtimestamp(lastUpdatedInt, timezone.utc)
+            lastUpdatedFormatted = lastUpdatedDate.strftime("%B %d, %Y")
+           
         else:
             return {"error": "No resource found"}       
 
@@ -133,10 +97,10 @@ async def get_data(topic_id: int):
             "LastUpdated": lastUpdatedFormatted,
             "title": title,
             "link": link,
-            "content": clean_content
         }
-        print(json.dumps(data, indent=5))
-        # return sections
+        # Debug
+        # print(json.dumps(data, indent=5))
+        return data
     else:
         print({"error": "Failed to fetch data from the health.gov API"})
 
@@ -168,16 +132,11 @@ async def get_list_and_random(
         "TotalIds": len(ids),
         "RandomId": random_id,
     }
-    
-    print(json.dumps(data, indent=4))
+    # Debug
+    # print(json.dumps(data, indent=4))
     return data
 
-@app.get("/health_tips")
-async def health_tips(param: str):
-    data = await get_list_and_random(param)
-    id = data.get("RandomId")
-    result = await get_data(id)
-    return result
+# ---------End copy here----------
 
 async def main():
   await health_tips(25) 
